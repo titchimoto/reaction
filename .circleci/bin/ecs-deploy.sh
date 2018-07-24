aec2c930162ae5446e67536690ca2cf3ba236fd0
griggheo <grig@gheorghiu.net>
2018-07-24 16:14:35 -0700

    do not use sops in CircleCI scripts

--- .circleci/bin/ecs-deploy.sh
+++ .circleci/bin/ecs-deploy.sh
@@ -6,8 +6,8 @@ wget https://github.com/mikefarah/yq/releases/download/2.0.1/yq_linux_amd64 -O /
 sudo mv /tmp/yq /usr/local/bin/yq
 sudo chmod +x /usr/local/bin/yq
 
-wget https://github.com/mozilla/sops/releases/download/3.0.5/sops_3.0.4_amd64.deb -O /tmp/sops_3.0.4_amd64.deb
-sudo dpkg -i /tmp/sops_3.0.4_amd64.deb
+#wget https://github.com/mozilla/sops/releases/download/3.0.5/sops_3.0.4_amd64.deb -O /tmp/sops_3.0.4_amd64.deb
+#sudo dpkg -i /tmp/sops_3.0.4_amd64.deb
 
 cd devops/aws/app
-source <(sops -d core-service/.env.sops) && ./update-app-stack.sh core-service
+./update-app-stack.sh core-service
