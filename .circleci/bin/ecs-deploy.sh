fd9a26047c67a74ddb0ecb1e9cf60c09ecc4cb74
griggheo <grig@gheorghiu.net>
2018-07-24 15:41:47 -0700

    use sudo for moving yq binary

--- .circleci/bin/ecs-deploy.sh
+++ .circleci/bin/ecs-deploy.sh
@@ -2,7 +2,8 @@
 
 sudo apt-get -y install python3-pip wget
 sudo pip3 install awscli
-wget https://github.com/mikefarah/yq/releases/download/2.0.1/yq_linux_amd64 -O /usr/local/bin/yq
+wget https://github.com/mikefarah/yq/releases/download/2.0.1/yq_linux_amd64 -O /tmp/yq
+sudo mv /tmp/yq /usr/local/bin/yq
 sudo chmod +x /usr/local/bin/yq
 
 wget https://github.com/mozilla/sops/releases/download/3.0.5/sops_3.0.4_amd64.deb -O /tmp/sops_3.0.4_amd64.deb
